841b596efaf816f662cba96902acf1eb986855a6
Egon Okerman <dev@egon.me>
2021-11-28 21:44:51 +0100

    Split up into separate modules

---
+++ aoc-notifier/formatter.py
@@ -0,0 +1,20 @@
+import datetime
+
+import inflect
+
+INFLECT = inflect.engine()
+
+
+def format_leaderboard_changes(leaderboard_changes):
+    result = ""
+    for change in leaderboard_changes:
+        result += f"{':arrow_up:' if change['positive'] else ':arrow_down:'} *{INFLECT.ordinal(change['position'])}* {change['username']} ({change['points']} points)\n"
+
+    return result
+
+
+def format_complete_chal_message(challenge):
+    star = ":star2:" if challenge["part"] == 2 else ":star:"
+    timestamp = datetime.datetime.fromtimestamp(challenge['timestamp'])
+
+    return f"*{challenge['username']}* has completed {star} *Day {challenge['day']}* ({challenge['part']}) at {timestamp.strftime('%H:%M:%S')}!"
